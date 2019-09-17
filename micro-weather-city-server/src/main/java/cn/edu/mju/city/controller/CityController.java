9cdb1ccf9fee26928282ef28fced989b4198f50a
zengchengxie <zengchengxie@foxmail.com>
2019-09-17 23:07:23 +0800

    增加了Hystrix熔断器

--- micro-weather-city-server/src/main/java/cn/edu/mju/city/controller/CityController.java
+++ micro-weather-city-server/src/main/java/cn/edu/mju/city/controller/CityController.java
@@ -2,12 +2,14 @@ package cn.edu.mju.city.controller;
 
 import cn.edu.mju.city.entity.City;
 import cn.edu.mju.city.service.CityDataService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,6 +30,7 @@ public class CityController {
      * @return
      */
     @GetMapping
+    @HystrixCommand(fallbackMethod = "fallback")
     public List<City> listCity() throws RuntimeException {
 
         List<City> cityList = null;
@@ -39,5 +42,23 @@ public class CityController {
         }
         return cityList;
     }
+
+    /**
+     * 熔断默认返回的方法
+     * @return
+     */
+    public List<City> fallback(){
+
+        List<City> cityList = new ArrayList<>();
+        City city = new City();
+        city.setCityName("福州");
+        city.setCityCode("1102454");
+        city.setCityId("2120245");
+        city.setProvince("福建");
+        cityList.add(city);
+        return cityList;
+
+    }
+
 
 }
